6d902fc5eeaefb09599ad36023ba0c5cfcb99caa
ryansin937 <ryansin937@gmail.com>
2021-11-09 23:15:57 +0900

    feat : v2 완료

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,16 +1,100 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-  private userService: UserService;
-
-  constructor(userService: UserService) {
-    this.userService = userService;
-  }
+  constructor(private readonly userService: UserService) {}
 
   @Get() //경로를 설정하지 않으면 "user/" 경로로 설정이 된다.
   getHelloWorld(): string {
     return this.userService.getHelloWorld();
+  }
+
+  /**
+   * @author Ryan
+   * @description @Body 방식 - @Body 어노테이션 여러개를 통해 요청 객체를 접근할 수 있습니다.
+   *
+   * @param id 유저 고유 아이디
+   * @param name 유저 이름
+   */
+  @Post('/create_user')
+  onCreateUser(@Body('id') id: number, @Body('name') name: string): User[] {
+    return this.userService.onCreateUser(id, name);
+  }
+
+  /**
+   * @author Ryan
+   * @description 전체 유저 조회
+   */
+  @Get('/user_all')
+  getUserAll(): User[] {
+    return this.userService.getUserAll();
+  }
+
+  /**
+   * @author Ryan
+   * @description @Query 방식 - 단일 유저 조회
+   *
+   * @param id 유저 고유 아이디
+   */
+  @Get('/user')
+  findByUserOne1(@Query('id') id: number): User {
+    return this.userService.findByUserOne(id);
+  }
+
+  /**
+   * @author Ryan
+   * @description @Param 방식 - 단일 유저 조회
+   *
+   * @param id 유저 고유 아이디
+   */
+  @Get('/user/:id')
+  findByUserOne2(@Param('id') id: number): User {
+    return this.userService.findByUserOne(id);
+  }
+
+  /**
+   * @author Ryan
+   * @description @Param & @Body 혼합 방식 - 단일 유저 수정
+   *
+   * @param id 유저 고유 아이디
+   * @param name 유저 이름
+   */
+  @Patch('/user/:id')
+  setUser(@Param('id') id: number, @Body('name') name: string): User {
+    return this.userService.setUser(id, name);
+  }
+
+  /**
+   * @author Ryan
+   * @description @Param & @Body 혼합 방식 - 전체 유저 수정
+   *
+   * @param id 유저 고유 아이디
+   * @param name 유저 이름
+   */
+  @Put('/user/update')
+  setAllUser(@Body('id') id: number, @Body('name') name: string): User[] {
+    return this.userService.setAllUser(id, name);
+  }
+
+  /**
+   * @author Ryan
+   * @description @Query 방식 - 단일 유저 삭제
+   *
+   * @param id 유저 고유 아이디
+   */
+  @Delete('/user/delete')
+  deleteUser(@Query('id') id: number): User[] {
+    return this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,88 @@
 import { Injectable } from '@nestjs/common';
 
+const users: User[] = [
+  { id: 1, name: '유저1' },
+  { id: 2, name: '유저2' },
+  { id: 3, name: '유저3' },
+];
+
 @Injectable()
 export class UserService {
+  /**
+   * @author Ryan
+   * @description 유저 생성
+   *
+   * @param id 유저 고유 아이디
+   * @param name 유저 이름
+   * @returns {User[]} users
+   */
+  onCreateUser(id: number, name: string): User[] {
+    return users.concat({ id, name });
+  }
+
+  /**
+   * @author Ryan
+   * @description 모든 유저 조회
+   *
+   * @returns {User[]} users
+   */
+  getUserAll(): User[] {
+    return users;
+  }
+
+  /**
+   * @author Ryan
+   * @description 단일 유저 조회
+   *
+   * @param id 유저 고유 아이디
+   * @returns {User} users
+   */
+  findByUserOne(id: number): User {
+    return users.find((data) => data.id == id);
+  }
+
+  /**
+   * @author Ryan
+   * @description 단일 유저 수정
+   *
+   * @returns {User} users
+   */
+  setUser(id: number, name: string): User {
+    return users.find((data) => {
+      if (data.id == id) return (data.name = name);
+    });
+  }
+
+  /**
+   * @author Ryan
+   * @description 전체 유저 수정
+   *
+   * @returns {User[]} users
+   */
+  setAllUser(id, name): User[] {
+    return users.map((data) => {
+      if (data.id == id) {
+        data.name = name;
+      }
+
+      return {
+        id: data.id,
+        name: data.name,
+      };
+    });
+  }
+
+  /**
+   * @author Ryan
+   * @description 유저 삭제
+   *
+   * @param id
+   * @returns {User[]} users
+   */
+  deleteUser(id: number): User[] {
+    return users.filter((data) => data.id != id);
+  }
+
   getHelloWorld(): string {
     return 'Hello World!!';
   }
