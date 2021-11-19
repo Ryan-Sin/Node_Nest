bf6c7b94a4daf8653fa1aa7bcff4b5a0e2d941dd
ryansin937 <ryansin937@gmail.com>
2021-11-20 02:05:14 +0900

    feat : v8 완료

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,22 @@ import { LocalStrategy } from './strategies/auth.local.strategy';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from 'src/repository/user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './strategies/auth.jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository]), PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      //토큰 서명 값 설정
+      secret: jwtConstants.secret,
+      //토큰 유효시간 (임의 60초)
+      signOptions: { expiresIn: '60s' },
+    }),
+    TypeOrmModule.forFeature([UserRepository]),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
